b7f70a58c990a3ff9ca2ad748e0703a8bb979830
Makki <almakki0909099@gmail.com>
2020-01-03 14:56:38 -0600

    Use StingUtils and added dependecy to lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,13 +3,24 @@ package clinic.programming.training;
 import java.util.ArrayList; // implementation of that interface
 import java.util.List; // interface
 
+// This imports the "StringUtils" class from the "apache.commons.lang3" library. 
+import org.apache.commons.lang3.StringUtils; 
+
 public class Application {
+	
+	// countWords method will simply accept a string containing words separated by spaces
+	// and it will return the integer representing how many words are in that string.
+	public int countWords(String words) {
+		//This is to utilize the "StringUtils" class, and the "split" method on that.
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0: separateWords.length;
+	}
 	public void great() {
 		List <String> greetings = new ArrayList<>(); //generics list
 		greetings.add("Hello");
 		
 		for (String greeting : greetings) {
-			System.out.println("Greeting :" + greeting);
+			System.out.println("Greetings :" + greeting);
 			
 		}
 	}
@@ -18,10 +29,17 @@ public class Application {
         System.out.println ("Inside Application");
     }
 
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.great();
+	
+		// countWords method, passing in the string "I have four words".
+		// I'm capturing the result into an integer called count,which then I use
+		// in the "System.out.println". Let's save our changes and then close.
+		int count =app.countWords(" I have four words ");
+		System.out.println("Word Count: " + count);
     }
 }
